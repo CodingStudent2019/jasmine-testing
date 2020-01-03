7b5881c92eea44b6c4d32947d106e417d1a6aee3
CodingStudent2019 <tejalsaldanhaone@outlook.com>
2020-01-03 11:52:45 +0000

    jasmine testing

--- jasmine-testing/spec/calcSpec.js
+++ jasmine-testing/spec/calcSpec.js
@@ -1,6 +1,6 @@
-describe("Calculator", function () {
-    describe("Addition tests", function () {
-        it("should  return 42", function () {
+describe("Calculator", function() {
+    describe("Addition tests", function() {
+        it("should return 42", function() {
             expect(addition(20, 22)).toBe(42);
         });
     });
