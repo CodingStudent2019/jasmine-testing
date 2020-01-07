8f3d44c1821248e42ef18e09e24b93aa1910f13b
CodingStudent2019 <tejalsaldanhaone@outlook.com>
2020-01-07 19:47:06 +0000

    jasmine testing

--- jasmine-testing/scripts/drinks.js
+++ jasmine-testing/scripts/drinks.js
@@ -2,16 +2,16 @@ whatCanIDrink = function(age) {
   if (age < 0) {
       return "Sorry. I can’t tell what drink because that age is incorrect!"
   }
-   if (age < 14) {
+   else if (age < 14) {
        return "Drink Toddy"
    }
-   if (age < 18) {
+   else if (age < 18 && age >= 14) {
        return "Drink Coke"
    }
-   if (age < 21) {
+   else if (age < 21 && age >= 18) {
        return "Drink Beer"
    }
-   if (age < 130) {
+   else if (age < 130) {
        return "Drink Whisky"
    }
    else {
